dde09334532c52cabc827c2082e6ce505518a8a4
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-03-19 09:34:23 -0400

    First commit photo-albm

---
+++ r2013-03-19-photo-albm/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+R20130319PhotoAlbm::Application.config.secret_token = '84fe28e7557b2c9b59085e208e462c56775fb3fbd49dd328c9c307d26a93fed7298dd455e2adcae362cf2fe756b1d4b9f340f6786fbd2dc45b5bea382e757b0e'
